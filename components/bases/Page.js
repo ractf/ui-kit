502ac5c936369c73892d251528288f00910e694a
Bottersnike <bottersnike237@gmail.com>
2020-07-10 03:28:57 +0100

    Fix page growing

--- components/bases/Page.js
+++ components/bases/Page.js
@@ -4,11 +4,13 @@ import { makeClass } from "@ractf/util";
 import style from "./Page.module.scss";
 
 
-const Page = ({ centre, title, children }) => {
+const Page = ({ centre, title, noWrap, children }) => {
     useEffect(() => {
         if (title) document.title = title;
     }, [title]);
-    
+
+    if (noWrap) return children;
+
     return <div className={makeClass(style.pageContent, centre && style.centre)}>
         {children}
     </div>;
