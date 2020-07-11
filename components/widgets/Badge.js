8dde89c973747a52265320a3294245dfc399df6f
Bottersnike <bottersnike237@gmail.com>
2020-07-11 12:44:09 +0100

    Make badges quittable

--- components/widgets/Badge.js
+++ components/widgets/Badge.js
@@ -4,10 +4,11 @@ import { makeClass, propsToTypeClass } from "@ractf/util";
 import style from "./Badge.module.scss";
 
 
-const Badge = ({ children, ...props }) => {
+const Badge = ({ children, onClose, ...props }) => {
     const extra = propsToTypeClass(props, style, "primary");
 
-    return <div className={makeClass(style.badge, props.pill && style.pill, extra)}>
+    return <div className={makeClass(style.badge, props.x && style.x, props.pill && style.pill, extra)}>
+        {props.x && <div onClick={onClose} />}
         {children}
     </div>;
 };
