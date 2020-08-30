d5afa8a27834c99e0536e2cd54ae239d704a8eff
Bottersnike <bottersnike237@gmail.com>
2020-08-30 19:25:20 +0100

    Add tab support for multi-line inputs

--- components/forms/Input.js
+++ components/forms/Input.js
@@ -46,7 +46,7 @@ export const InputTags = ({ disabled, val, onChange }) => {
             setNewTag("");
         } else if (e.keyCode === 32) {
             if (newTag.length === 0)
-            e.preventDefault();
+                e.preventDefault();
         } else if (e.keyCode === 8) {
             if (newTag.length === 0) {
                 e.preventDefault();
@@ -94,8 +94,8 @@ export class RawInput extends PureComponent {
         this.isInput = true;
     }
 
-    handleChange(event) {
-        let value = event.target.value;
+    handleChange(event, value) {
+        value = value || event.target.value;
         // Length limit
         if (this.props.limit)
             value = value.substring(0, this.props.limit);
@@ -118,11 +118,103 @@ export class RawInput extends PureComponent {
         this.setState({ showPass: !this.state.showPass });
     }
 
+    insertString = (string) => {
+        const elem = this.inputRef.current;
+        const s = elem.selectionStart;
+        elem.value = (
+            elem.value.substring(0, elem.selectionStart) + string + elem.value.substring(elem.selectionEnd)
+        );
+        elem.selectionEnd = s + string.length;
+        this.handleChange({ target: elem }, elem.value);
+    }
+
+    currentLine = () => {
+        if (!this.props.rows)
+            return this.state.val;
+        
+        const elem = this.inputRef.current;
+        const lines = elem.value.substring(0, elem.selectionStart).split("\n");
+        console.log(lines);
+        return lines[lines.length - 1];
+    }
+
+    getSelectedLines = () => {
+        const elem = this.inputRef.current;
+        const lines = [];
+        let line = 0;
+        const start = elem.selectionStart;
+        const end = elem.selectionEnd;
+
+        for (const [i, n] of [...elem.value].map((i, n) => [i, n])) {
+            if (n > end) {
+                return lines;
+            } else if (n >= start) {
+                if (lines.indexOf(line) === -1) {
+                    lines.push(line);
+                }
+            }
+            if (i === "\n") line++;
+        }
+        return lines;
+    }
+
+    tabUnindent = () => {
+        const elem = this.inputRef.current;
+        
+        const selectedLines = this.getSelectedLines();
+        const text = elem.value.split("\n");
+        for (const i of selectedLines) {
+            const match = /^([\t ]{0,4})/.exec(text[i])[0];
+            console.log(match, i);
+            text[i] = text[i].replace(/^([\t ]{0,4})/, "");
+        }
+        let newStart = 0, newEnd = -1;
+        for (const n in text) {
+            if (n < selectedLines[0]) newStart += text[n].length + 1;
+            if (n <= selectedLines[selectedLines.length - 1]) newEnd += text[n].length + 1;
+        }
+
+        elem.value = text.join("\n");
+        this.handleChange({ target: elem }, elem.value);
+        elem.setSelectionRange(newStart, newEnd);
+    }
+
+    tabIndent = () => {
+        const elem = this.inputRef.current;
+        if (elem.selectionStart === elem.selectionEnd)
+            return this.insertString("    ");
+        
+        const selectedLines = this.getSelectedLines();
+        const text = elem.value.split("\n");
+        for (const i of selectedLines) {
+            text[i] = "    " + text[i];
+        }
+        let newStart = 0, newEnd = -1;
+        for (const n in text) {
+            if (n < selectedLines[0]) newStart += text[n].length + 1;
+            if (n <= selectedLines[selectedLines.length - 1]) newEnd += text[n].length + 1;
+        }
+
+        elem.value = text.join("\n");
+        this.handleChange({ target: elem }, elem.value);
+        elem.setSelectionRange(newStart, newEnd);
+    }
+
     keyDown = (e) => {
         if (e.keyCode === 13) {
             e.preventDefault();
-            if (this.props.onSubmit)
+            if (this.props.rows) {
+                const line = this.currentLine();
+                console.log(line);
+                this.insertString("\n" + /^([ \t]*)/.exec(line)[0]);
+            } else if (this.props.onSubmit)
                 this.props.onSubmit(this.props.val);
+        } else if (e.keyCode === 9 && this.props.monospace) {
+            e.preventDefault();
+            if (e.shiftKey)
+                this.tabUnindent();
+            else
+                this.tabIndent();
         }
     }
 
@@ -146,6 +238,7 @@ export class RawInput extends PureComponent {
         return <div className={inputClass} onClick={this.click}>
             {this.props.rows ?
                 <textarea
+                    onKeyDown={this.keyDown}
                     ref={this.inputRef}
                     value={this.props.val}
                     onChange={this.handleChange}
@@ -166,7 +259,7 @@ export class RawInput extends PureComponent {
                     className={makeClass(style.input, this.props.monospace && style.monospaced)}
                     autoFocus={this.props.autoFocus}
                     disabled={this.props.disabled || this.props.readonly} />}
-            {this.props.limit && (
+            {(this.props.limit || 1) && (
                 <div className={style.lengthCounter}>{this.props.val?.length}/{this.props.limit}</div>
             )}
             {this.props.password ? <div className={style.styledEye} onClick={this.togglePwd}>
