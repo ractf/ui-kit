132ec11b347b25ca35ce2efe10d04bf28a6898b2
Bottersnike <bottersnike237@gmail.com>
2020-12-19 22:09:31 +0000

    Clean up centring of pages

--- components/bases/Page.js
+++ components/bases/Page.js
@@ -4,14 +4,14 @@ import { makeClass } from "@ractf/util";
 import style from "./Page.module.scss";
 
 
-const Page = ({ centre, title, noWrap, children }) => {
+const Page = ({ centre, title, noWrap, children, className }) => {
     useEffect(() => {
         if (title) document.title = title;
     }, [title]);
 
     if (noWrap) return children;
 
-    return <div className={makeClass(style.pageContent, centre && style.centre)}>
+    return <div className={makeClass(style.pageContent, centre && style.centre, className)}>
         {children}
     </div>;
 };
