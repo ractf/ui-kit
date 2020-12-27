10b6c845db7b9c7a275ee744d17b1c4b371710ff
Bottersnike <bottersnike237@gmail.com>
2020-12-27 17:59:33 +0000

    Fix file dragging

--- components/forms/FileUpload.js
+++ components/forms/FileUpload.js
@@ -12,6 +12,18 @@ const FileUpload = ({ name, onChange, globalDrag, error }) => {
     const timeoutRef = useRef();
     const inputRef = useRef();
 
+    const localOnChange = useCallback((e) => {
+        const files = e.target.files;
+        if (files.length === 0) {
+            if (onChange)
+                onChange(null);
+            setFilename(null);
+        } else {
+            if (onChange)
+                onChange(files[0]);
+            setFilename(files[0].name);
+        }
+    }, [onChange]);
     useEffect(() => {
         if (globalDrag) {
             const onDragOver = (e) => {
@@ -28,41 +40,36 @@ const FileUpload = ({ name, onChange, globalDrag, error }) => {
                     setOver(false);
                 }, 100);
             };
+            const onDrop = (e) => {
+                inputRef.current.files = e.dataTransfer.files;
+                e.preventDefault();
+                if (timeoutRef.current)
+                    clearTimeout(timeoutRef.current);
+                setOver(false);
+                localOnChange({ target: { files: e.dataTransfer.files }});
+            };
 
             document.addEventListener("dragover", onDragOver);
             document.addEventListener("dragleave", onDragExit);
+            document.addEventListener("drop", onDrop);
             return () => {
                 document.removeEventListener("dragover", onDragOver);
                 document.removeEventListener("dragleave", onDragExit);
+                document.removeEventListener("drop", onDrop);
                 if (timeoutRef.current)
                     clearTimeout(timeoutRef.current);
             };
         }
-    }, [globalDrag]);
-
-    const localOnChange = useCallback((e) => {
-        const files = e.target.files;
-        if (files.length === 0) {
-            if (onChange)
-                onChange(null);
-            setFilename(null);
-        } else {
-            if (onChange)
-                onChange(files[0]);
-            setFilename(files[0].name);
-        }
-    }, [onChange]);
+    }, [globalDrag, localOnChange]);
     const onClick = useCallback(() => {
         inputRef.current.click();
     }, []);
 
     return <>
         {isOver && (
-            <label htmlFor={name} class={style.hoverOverlayWrap}>
-                <div class={style.hoverOverlay}>
-                    <span>Drop file anywhere to upload</span>
-                </div>
-            </label>
+            <div htmlFor={name} class={style.hoverOverlay}>
+                <span>Drop file anywhere to upload</span>
+            </div>
         )}
         <input onChange={localOnChange} ref={inputRef} id={name} name={name}
             type="file" style={{ display: "none" }} />
